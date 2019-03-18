fd027bd01e9d48125c67192b6e54a70aa7cc5410
Jon Jagger <jon@jaggersoft.com>
2019-03-18 09:52:04 +0000

    refactoring; script tidying

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -5,11 +5,11 @@ readonly GITHUB_ORG=https://raw.githubusercontent.com/cyber-dojo
 readonly CREATE_IMAGE_SCRIPT=cyber_dojo_start_points_create.sh
 readonly TMP_DIR=$(mktemp -d /tmp/cyber-dojo-exercises.XXXXXXXXX)
 
-cleanup() { rm -rf ${TMP_DIR} > /dev/null; }
-trap cleanup EXIT
+remove_tmp_dir() { rm -rf ${TMP_DIR} > /dev/null; }
+trap remove_tmp_dir EXIT
 
 cd ${TMP_DIR}
-curl -O "${GITHUB_ORG}/starter-base/master/${CREATE_IMAGE_SCRIPT}"
+curl -O --silent "${GITHUB_ORG}/starter-base/master/${CREATE_IMAGE_SCRIPT}"
 chmod 700 ./${CREATE_IMAGE_SCRIPT}
 
 ./${CREATE_IMAGE_SCRIPT} \
