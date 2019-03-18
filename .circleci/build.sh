1c9bfc06d477d28bf6063d788cb6da7ca6a12154
Jon Jagger <jon@jaggersoft.com>
2019-03-18 09:44:54 +0000

    switch to use new starter-base repo

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -3,17 +3,16 @@ set -ev
 
 readonly GITHUB_ORG=https://raw.githubusercontent.com/cyber-dojo
 readonly CREATE_IMAGE_SCRIPT=cyber_dojo_start_points_create.sh
-readonly IMAGE_NAME=cyberdojo/exercises
 readonly TMP_DIR=$(mktemp -d /tmp/cyber-dojo-exercises.XXXXXXXXX)
 
 cleanup() { rm -rf ${TMP_DIR} > /dev/null; }
 trap cleanup EXIT
 
 cd ${TMP_DIR}
-curl -O "${GITHUB_ORG}/start-points-base/master/${CREATE_IMAGE_SCRIPT}"
+curl -O "${GITHUB_ORG}/starter-base/master/${CREATE_IMAGE_SCRIPT}"
 chmod 700 ./${CREATE_IMAGE_SCRIPT}
 
 ./${CREATE_IMAGE_SCRIPT} \
-    ${IMAGE_NAME} \
-    --exercises \
-      https://github.com/cyber-dojo/exercises.git
+    cyberdojo/exercises \
+      --exercises \
+        https://github.com/cyber-dojo/exercises.git
