8734996b07f6c7ff51ecc541a3a9c0d832723828
Jon Jagger <jon@jaggersoft.com>
2019-03-29 12:06:02 +0000

    refactoring; delete dead code

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -14,7 +14,6 @@ curl -O --silent "${GITHUB_ORG}/starter-base/master/${SCRIPT_NAME}"
 #curl -O --silent "${GITHUB_ORG}/commander/master/${SCRIPT_NAME}"
 chmod 700 ./${SCRIPT_NAME}
 
-#./${SCRIPT_NAME} start-point create \
 ./${SCRIPT_NAME} start-point create \
     cyberdojo/exercises \
       --exercises \
