667b9884238043b1f89e0ee78db87d9dbfaeacfd
Jon Jagger <jon@jaggersoft.com>
2019-03-29 13:54:23 +0000

    prepare for switch to commander's cyber-dojo script

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -2,16 +2,16 @@
 set -ev
 
 readonly GITHUB_ORG=https://raw.githubusercontent.com/cyber-dojo
-readonly SCRIPT_NAME=cyber_dojo_start_points_create.sh
-#readonly SCRIPT_NAME=cyber-dojo
+readonly REPO_NAME=starter-base
+#readonly REPO_NAME=commander
+readonly SCRIPT_NAME=cyber-dojo
 readonly TMP_DIR=$(mktemp -d /tmp/cyber-dojo-exercises.XXXXXXXXX)
 
 remove_tmp_dir() { rm -rf ${TMP_DIR} > /dev/null; }
 trap remove_tmp_dir EXIT
 
 cd ${TMP_DIR}
-curl -O --silent "${GITHUB_ORG}/starter-base/master/${SCRIPT_NAME}"
-#curl -O --silent "${GITHUB_ORG}/commander/master/${SCRIPT_NAME}"
+curl -O --silent "${GITHUB_ORG}/${REPO_NAME}/master/${SCRIPT_NAME}"
 chmod 700 ./${SCRIPT_NAME}
 
 ./${SCRIPT_NAME} start-point create \
