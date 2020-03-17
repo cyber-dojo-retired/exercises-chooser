757f9aeb3a83bde6a9079561c189bb614747e3ea
Jon Jagger <jon@jaggersoft.com>
2020-03-17 15:53:51 +0000

    Set k8s helm yml values for image/port from cyberdojo/versioner

--- .circleci/deploy_to_namespace.sh
+++ .circleci/deploy_to_namespace.sh
@@ -1,6 +1,13 @@
 #!/bin/bash -Eeu
 
+readonly SH_DIR="$(cd "$(dirname "${0}")/../sh" && pwd)"
+source ${SH_DIR}/versioner_env_vars.sh
+export $(versioner_env_vars)
+
 readonly NAMESPACE="${1}" # eg beta
+readonly IMAGE="${CYBER_DOJO_EXERCISES_CHOOSER_IMAGE}"
+readonly PORT="${CYBER_DOJO_EXERCISES_CHOOSER_PORT}"
+readonly TAG="${CIRCLE_SHA1:0:7}"
 
 # misc env-vars are in ci context
 
@@ -9,18 +16,24 @@ echo ${GCP_K8S_CREDENTIALS} > /gcp/gcp-credentials.json
 gcloud auth activate-service-account \
   "${SERVICE_ACCOUNT}" \
   --key-file=/gcp/gcp-credentials.json
+
 gcloud container clusters get-credentials \
   "${CLUSTER}" \
   --zone "${ZONE}" \
   --project "${PROJECT}"
 
 helm init --client-only
+
 helm repo add praqma https://praqma-helm-repo.s3.amazonaws.com/
-TAG="${CIRCLE_SHA1:0:7}"
+
 helm upgrade \
   --install \
   --namespace=${NAMESPACE} \
+  --set-string containers[0].image=${IMAGE} \
   --set-string containers[0].tag=${TAG} \
+  --set service.port=${PORT} \
+  --set containers[0].livenessProbe.port=${PORT} \
+  --set containers[0].readinessProbe.port=${PORT} \
   --values .circleci/exercises-chooser-values.yaml \
   ${NAMESPACE}-exercises-chooser \
   praqma/cyber-dojo-service \
